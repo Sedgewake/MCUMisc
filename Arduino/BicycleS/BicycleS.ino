643a96026a638c752216a0b2ae4023fa39482f02
Sedgewake <evansj5125@gmail.com>
2025-06-28 08:32:21 -0700

    2025.06.28 08:32

--- Arduino/BicycleS/BicycleS.ino
+++ Arduino/BicycleS/BicycleS.ino
@@ -14,8 +14,8 @@ Adafruit_SH1106G display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 const int HALL_SENSOR_PIN = 3;
 
-const float VAL_1 = 8043.48f;
-const float WHEEL_CIRCUMFERENCE_M = 2.2343f;
+const float VAL_1 = 8029.8f;
+const float WHEEL_CIRCUMFERENCE_M = 2.2305f;
 volatile unsigned long lastHallTriggerTime = 0;
 volatile unsigned long currentTime = 0;
 volatile unsigned long rotationTime = 0;
